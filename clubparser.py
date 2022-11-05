cc519e9c0328c155121e0b8212680beb096931fe
Anderson Olson <92558834+AOlson523@users.noreply.github.com>
2022-11-05 16:50:23 -0700

    Created type and subtype keys

---
+++ clubparser.py
@@ -0,0 +1,25 @@
+import json
+from bs4 import BeautifulSoup
+
+with open("zothacksf22.html", encoding="utf8") as htmltext:
+    datasoup = BeautifulSoup(htmltext, 'html.parser')
+
+    clubs_json_list = []
+    club_groups = datasoup.find_all('h2', class_ = "media-heading header-cg--h4")
+    for club in club_groups:
+        club_info = club.find_all('a', limit = 1)
+        club_type = club.find_all('p', class_ = 'h5 media-heading grey-element')
+        club_type = club_type[0].get_text(strip = True)
+        if '-' in club_type:
+            club_type = club_type.split('-')
+            club_main_type = club_type[0].strip()
+            club_sub_type = club_type[1].strip().split(', ')
+        else:
+            club_main_type = club_type
+            club_sub_type = None
+        clubs_json_list.append({'name': club_info[0].get_text(strip = True), 'main_type': club_main_type, 'sub_type': club_sub_type})
+    
+    clubs_json = json.dumps(clubs_json_list, indent = 4)
+
+    with open('campus_groups.json', 'w') as output:
+        output.write(clubs_json)
